2ceec78093bd0a25f9ce521bf52fa343080a0312
Hasanuzzaman <shuvo009@yahoo.com>
2012-07-02 22:03:26 +0600

    RadioButtobBindingDemo

---
+++ RadioButtonBindingDemo/ConverterRadioButtom.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace RadioButtonBindingDemo
+{
+    class ConverterRadioButtom :IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value!=null || parameter !=null)
+            {
+                string checkValue = value.ToString();
+                string targetValue = parameter.ToString();
+                return checkValue.Equals(targetValue, StringComparison.InvariantCultureIgnoreCase);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return null;
+            bool useValue = (bool)value;
+            string targetValue = parameter.ToString();
+            if (useValue)
+                return parameter;
+
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ RadioButtonBindingDemo/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace RadioButtonBindingDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _SelectedRadioButtom = "RadioButtonOne";
+
+        public string SelectedRadioButtom
+        {
+            get { return _SelectedRadioButtom; }
+            set
+            {
+                if (_SelectedRadioButtom == value)
+                    return;
+                _SelectedRadioButtom = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("SelectedRadioButtom"));
+            }
+        }
+        
+    }
+}
